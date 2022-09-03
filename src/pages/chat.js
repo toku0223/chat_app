d623fbad7af8115c99a4d26c4f98da07fa7a2311
toku0223 <kei.nakanotoku@gmail.com>
2022-09-03 12:01:28 +0900

    main

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,20 +1,29 @@
+import Avatar from '@mui/material/Avatar';
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { db, messageData } from '../plugins/firebase';
+import "./chat.css";
+
 
 const Chat = () => {
     const location = useLocation();
     const state = location.state
 
     const [message, setMessage] = useState('')
-    const [userName, setUserName] = useState(state)
+    const [userName, setUserName] = useState(state.substring(0, state.indexOf("/")))
     const [dm, setDm] = useState([])
+    const [url, setURL] = useState(state.substring(state.indexOf("/") + 1))
 
     const messagesRef = collection(db, "Messages")
     useEffect(() => {
         // 名前とイメージURLの分離
+        console.log(state.substring(0, state.indexOf("/")))
+        console.log(state.substring(state.indexOf("/") + 1))
 
+        console.log(url)
+
+        // メッセージの受信
         const q = query(collection(db, "Messages"), orderBy("timestamp", "asc"));
         const unsubscribe = onSnapshot(q, (querySnapshot) => {
             const messages = [];
@@ -30,23 +39,23 @@ const Chat = () => {
     console.log(dm, "test")
     const q1 = query(messagesRef, orderBy("timestamp", "desc"))
 
-    console.log(state, "test2")
+
 
     const createMessage = async () => {
         console.log('ready')
-        const res = await messageData(message, userName)
+        const res = await messageData(message, userName, url)
         console.log(userName)
         console.log('complete', res)
     }
 
     return (
         <>
-
             <ul>
                 {
                     dm.map((d, index) => {
                         return (
                             <li key={index}>
+                                <Avatar alt="Remy Sharp" src={d.avatarURL} />
                                 <p>{d.userName}</p>
                                 <p>{d.message}</p>
                             </li>
